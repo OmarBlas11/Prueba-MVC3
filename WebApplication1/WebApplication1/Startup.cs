efee095deeefcda141aeeb6d37eee219f1bc04f7
omar blas loarte <blasloarte@outlook.es>
2019-09-23 23:25:49 -0500

    llevado a producción

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -42,7 +42,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsProduction())
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
